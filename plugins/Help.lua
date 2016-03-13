7a35c7f39ccdc03c564a59c8f60a5845fcb62933
Arman <thisiisarman@gmail.com>
2016-03-13 21:13:34 +0330

    TelegramLuaBot V2

--- plugins/Help.lua
+++ plugins/Help.lua
@@ -4,17 +4,20 @@ local triggers = {
 }
 
 local action = function(msg)
-    if string.match(msg.text, '^/help') then
+    
+    print('\n/help or /start', msg.from.first_name..' ['..msg.from.id..']')
     local text = ''
     if msg.chat.type == 'group' or msg.chat.type == 'supergroup' then
+        if is_owner(msg) then
         text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n/`/a YourPm!`\n'  --You Can Replace WELCOME TO ARMAN PV!
-
-        sendReply(msg, 'I\'ve send you the requested information in private.\nIf you have never used me before, please start a conversation with me and ask for help here again.')
+        end
+        sendReply(msg, '_I\'ve sent you the requested information in_ *private*.\n_If you have never used me before, please *start* me and ask for help here *again*._', true)
     end
     
     if msg.chat.type == 'private' then
-        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n`/a YourPm!`\n' --You Can Replace WELCOME TO ARMAN PV!
+        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n`/a YourPm!`\nthis bot is open sourced* [source](https://github.com/ThisIsArman/TelegramLuaBot)\n' --You Can Replace WELCOME TO ARMAN PV!
     end
+    
     sendMessage(msg.from.id, text, true, false, true)
 end
 
@@ -22,4 +25,3 @@ return {
 	action = action,
 	triggers = triggers
 }
-end
